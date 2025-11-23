a8e3d58b74ecab09c9577a1b5987d4908c10f777
mwalegacybuiltco-ops <mwalegacybuiltco@gmail.com>
2025-11-23 16:15:10 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// Simple placeholder service worker for PWA installability
-self.addEventListener('install',event=>self.skipWaiting());
-self.addEventListener('activate',event=>{});
-self.addEventListener('fetch',event=>{});
